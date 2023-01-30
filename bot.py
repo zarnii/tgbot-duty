8a8a4eb02b6cd3aa2de654e4eba48a42a5c5879c
zarnii <65334759+zarnii@users.noreply.github.com>
2023-01-31 01:12:18 +0300

    Сделаны обработчики кнопок

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, CallbackQuery
 from aiogram import Bot, Dispatcher, types, executor
 from config import TOKEN
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
 period_for_one_person = 0
+queue = []
 
 kb = ReplyKeyboardMarkup(keyboard=[
-	[KeyboardButton(text='Команды', callback_data='commands_btn'),KeyboardButton(text='Узнать период', callback_data='period_btn'),KeyboardButton(text='Узнать расписание', callback_data='duty_btn')]
+	[KeyboardButton(text='Команды'),KeyboardButton(text='Узнать период'),KeyboardButton(text='Узнать расписание')]
 ], resize_keyboard=True)
 
 @dp.message_handler(commands=['start'])
@@ -32,19 +36,31 @@ async def appoint_command(message: types.Message):
 	if period_for_one_person == 0:
 		await message.reply('Сперва назначте период дежурства для одного человека!')
 	else:
-		await message.reply(f'Задана очередь: {message.get_args()}')
+		if message.get_args() != '':
+			queue = message.get_args().split(' ')
+			print(queue)
+			await message.reply(f'Задана очередь: {message.get_args()}')
+		else: 
+			await message.reply(f'Укажите дежурных')
 
 
-'''
-@dp.callback_query_handler(text='commands_btn')
-async def ...
+#@dp.message_handler(commands=['pass']) фунция создания пропуска
 
-@dp.callback_query_handler(text='period_btn')
-async def ...
 
-@dp.callback_query_handler(text='duty_btn')
-async def ...
-'''
+@dp.message_handler(text='Команды')
+async def command_btn_pressed(message: types.Message):
+	await message.answer('/set_period - команда для установки периода дежуртсва для одного человека\n/appoint - создание списка дежурства')
+
+
+@dp.message_handler(text='Узнать период')
+async def period_btn_pressed(message: types.Message):
+	await message.answer(f'Заданный период: {period_for_one_person}')
+
+
+@dp.message_handler(text='Узнать расписание')
+async def duty_btn_pressed(message: types.Message):
+	await message.answer(f'Список дежурных: {queue}')
+
 
 @dp.message_handler()
 async def appoint_command(message: types.Message):
