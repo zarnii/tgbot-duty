b79b7a4776303025786fabfaa9d69a656338e5b5
Krakazyabra21 <98631177+Krakazyabra21@users.noreply.github.com>
2023-02-17 20:50:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 from datetime import datetime, timedelta
 from json_queue import JsonInterface
 
-q = JsonInterface()
-
 
 def make_duty(names: list, period: int) -> None:
+    q = JsonInterface()
+    for name in names:
+        q.enqueue(name, [])
+    dates_for_next = []
     k = 1
     holidays = q.get_holidays()
+    absence = q.get_absence()
     for name in names:
         dates = []
-        for j in range(period):
+        for days in dates_for_next:
+            dates.append(days)
+        print(dates)
+        dates_for_next = []
+        # for j in range(period - len(dates)):
+        while len(dates) < period:
             date = (datetime.now() + timedelta(days=k)).strftime("%d.%m")
+            date_miss = (datetime.now() + timedelta(days=k)).strftime("%d.%m.%Y")
+
+            date_add = (datetime.now() + timedelta(days=k)).strftime("%d.%m.%Y")
             if date not in holidays:
-                date_add = (datetime.now() + timedelta(days=k)).strftime("%d.%m.%Y")
-                k += 1
-                dates.append(date_add)
+                if date_miss not in absence[name]:
+                    k += 1
+                    dates.append(date_add)
+                else:
+                    k += 1
+                    dates_for_next.append(date_miss)
             else:
                 k += 1
-            q.enqueue(name, dates)
+        q.enqueue(name, dates)
 
 
 
