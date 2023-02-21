fa72fce2bfcdd3c3643cbe893ad7957bd96df6fa
LIefr1 <99041258+LIefr1@users.noreply.github.com>
2023-02-21 11:36:26 +0300

    Update date_vis1.py
    Поменял функции чтобы они брали данные из JsonInterface, и изменил их чтобы они возвращали готовое сообщение вместо данных.

--- date_vis1.py
+++ date_vis1.py
@@ -1,55 +1,50 @@
-import pandas as pd
 from datetime import datetime, timedelta
-import json
+from json_queue import JsonInterface
+
+queue = JsonInterface().cout_duty_queue()
 
 
-def open_file(file):
-    with open(file, 'r', encoding='UTF-8') as f:
-        data = json.load(f)
-    return data
-
-def get_tomorrow_shift(data:dict):
+def get_tomorrow_shift(data: dict):
     tomorrow = (datetime.now() + timedelta(days=1)).strftime("%d.%m.%Y")
-    data = data['duty_queue']
-    for key in data.keys():
+    mensaje = ''  # сообщение
+    for key in data:
         for value in data[key]:
-            if value[0] == tomorrow:
-                return key, value
+            if value == tomorrow:
+                try:
+                    mensaje += f"Завтра {value}, дежурит {key} "
+                except TypeError as error:
+                    print(error)
+                    mensaje += "На сегодня не назначен дежурный"
 
-def get_today_shift(data:dict):
+                return mensaje
+
+
+def get_today_shift(data: dict):
     today = datetime.now().strftime("%d.%m.%Y")
-    data = data['duty_queue']
-    for key in data.keys():
+    mensaje = ''
+    for key in data:
         for value in data[key]:
             if value == today:
-                return key, value
+                try:
+                    mensaje += f"Сегодня {value}, дежурит {key}"
+                except TypeError as error:
+                    print(error)
+                    mensaje += "На сегодня не назначен дежурный"
+                return mensaje
 
 
-def get_week(data:dict):
-    list = []
-    today = datetime.now().strftime("%d.%m.%Y")
-    data = data['duty_queue']
-    for key in data.keys():
+def get_week(data: dict):
+    date_n_worker = []
+
+    for key in data:
         for value in data[key]:
-            list.append((key, value))
+            date_n_worker.append((key, value))
 
-    return list
+    message = ''
+    for idx, val in enumerate(date_n_worker):
+        if idx == 7:
+            break
+        else:
+            message += f"Сегодня {val[1]}, дежурит {val[0]}\n"
 
-print(get_week(open_file('data.json')))
-
-ts = pd.date_range(open_file('data.json')['duty_queue']['zarnii'][0][0], periods=1)
-ts = pd.Series(ts).apply(str)
-new = ts.str.split(" ", n=1, expand=True)
-ts = new[0]
-month = ts.apply(lambda x: int(x[5:7]))
-mday = ts.apply(lambda x: int(x[8:10]))
-
-# print(open_file('data.json')['duty_queue'])
-# print(get_today_shift(open_file('data.json')))
-
-def week_day(date):
-    year, month, day = (int(x) for x in date.split('-'))
-    answer = date(year, month, day).weekday()
-    answer = int(answer) + 1
-    return answer
-
+    return message
